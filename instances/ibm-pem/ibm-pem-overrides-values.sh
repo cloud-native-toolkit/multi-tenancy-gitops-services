3c53d48e4f01af803a08d1e62719e7056a4af532
Budi Darmawan <vbudi@us.ibm.com>
2022-07-08 10:22:04 -0500

    remove secret from db
    Signed-off-by: Budi Darmawan <vbudi@us.ibm.com>

--- instances/ibm-pem/ibm-pem-overrides-values.sh
+++ instances/ibm-pem/ibm-pem-overrides-values.sh
@@ -9,6 +9,7 @@ B2BI_NONPROD=${B2BI_NONPROD:-"b2bi-nonprod"}
 PEM_VERSION=${PEM_VERSION:-"6.2.0.2"}
 RWX_STORAGECLASS=${RWX_STORAGECLASS:-"ibmc-file-gold"}
 CUSTOMER_ID=${CUSTOMER_ID:-"ibm@ibm.com"}
+PEM_SECRET=${PEM_SECRET:-"ibm-pem-secret"}
 
 # Create Kubernetes yaml
 ( echo "cat <<EOF" ; cat ibm-pem-overrides-values.yaml_template ;) | \
@@ -17,5 +18,6 @@ B2BI_PROD=${B2BI_PROD} \
 B2BI_NONPROD=${B2BI_NONPROD} \
 CUSTOMER_ID=${CUSTOMER_ID} \
 PEM_VERSION=${PEM_VERSION} \
+PEM_SECRET=${PEM_SECRET} \
 RWX_STORAGECLASS=${RWX_STORAGECLASS} \
 sh > values.yaml
