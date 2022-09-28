24b14dc0aed8f5f902937bcdcf20b5eb82c2d6d5
Knickkennedy <knicholas.kennedy@gmail.com>
2022-09-28 17:04:34 -0400

    added yaml_template setup and cleaned up helm charts to make sense
    Signed-off-by: Knickkennedy <knicholas.kennedy@gmail.com>

--- instances/sterling-secure-proxy-setup/run-setup.sh
+++ instances/sterling-secure-proxy-setup/run-setup.sh
@@ -45,8 +45,13 @@ if [[ -z ${ENGINE_CUSTOMCERT_PASSWORD} ]]; then
   exit 1
 fi
 
+if [[ -z ${RWX_STORAGECLASS}  ]]; then
+  echo "Please provide environment variable RWX_STORAGECLASS"
+  exit 1
+fi
+
 SEALED_SECRET_NAMESPACE=${SEALED_SECRET_NAMESPACE:-sealed-secrets}
-SEALED_SECRET_CONTOLLER_NAME=${SEALED_SECRET_CONTOLLER_NAME:-sealed-secrets}
+SEALED_SECRET_CONTROLLER_NAME=${SEALED_SECRET_CONTOLLER_NAME:-sealed-secrets}
 
 ### Configuration Manager
 printf "Creating Configuration Manager Secret \n"
@@ -61,11 +66,10 @@ oc create secret generic ibm-ssp-cm-secret --type=Opaque \
 --dry-run=client -o yaml > delete-ssp-cm-secret.yaml
 
 # Encrypt the secret using kubeseal and private key from the cluster
-kubeseal -n ${NS} --controller-name=${SEALED_SECRET_CONTOLLER_NAME} --controller-namespace=${SEALED_SECRET_NAMESPACE} -o yaml < delete-ssp-cm-secret.yaml > ibm-ssp-cm-secret.yaml
+kubeseal -n ${NS} --controller-name="${SEALED_SECRET_CONTROLLER_NAME}" --controller-namespace="${SEALED_SECRET_NAMESPACE}" -o yaml < delete-ssp-cm-secret.yaml
 
 # NOTE, do not check delete-ssp-cm-secret.yaml into git!
 rm delete-ssp-cm-secret.yaml
-
 ### Engine
 printf "Creating Engine secret \n"
 
@@ -78,7 +82,7 @@ oc create secret generic ibm-ssp-engine-secret --type=Opaque \
 --dry-run=client -o yaml > delete-ibm-ssp-engine-secret.yaml
 
 # Encrypt the secret using kubeseal and private key from the cluster
-kubeseal -n ${NS} --controller-name=${SEALED_SECRET_CONTOLLER_NAME} --controller-namespace=${SEALED_SECRET_NAMESPACE} -o yaml < delete-ibm-ssp-engine-secret.yaml > ibm-ssp-engine-secret.yaml
+kubeseal -n ${NS} --controller-name="${SEALED_SECRET_CONTROLLER_NAME}" --controller-namespace="${SEALED_SECRET_NAMESPACE}" -o yaml < delete-ibm-ssp-engine-secret.yaml
 
 # NOTE, do not check delete-ibm-ssp-engine-secret.yaml into git!
 rm delete-ibm-ssp-engine-secret.yaml
@@ -88,3 +92,9 @@ printf "Creating Service Accounts \n"
 oc create sa ssp-cm-serviceaccount -n "${NS}"
 oc create sa ssp-engine-serviceaccount -n "${NS}"
 oc create sa ssp-argocd-hook-job -n "${NS}"
+
+( echo "cat <<EOF" ; cat ssp-cm-pvc.yaml_template ;) | \
+sh > ssp-cm-pvc.yaml
+
+( echo "cat <<EOF" ; cat ssp-engine-pvc.yaml_template ;) | \
+sh > ssp-engine-pvc.yaml
