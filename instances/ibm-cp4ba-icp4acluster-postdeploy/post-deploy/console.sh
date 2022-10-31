873615fc123b4ca3819d5183f7fdf0a782db60f6
therayy <raafat@ibm.com>
2022-10-31 13:58:58 -0700

    markin cp4ba as default if not given
    Signed-off-by: therayy <raafat@ibm.com>

--- instances/ibm-cp4ba-icp4acluster-postdeploy/post-deploy/console.sh
+++ instances/ibm-cp4ba-icp4acluster-postdeploy/post-deploy/console.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 INGRESS_DOMAIN=$(oc get IngressController default -n openshift-ingress-operator -o jsonpath='{.status.domain}')
-NAMESPACE=${NAMESPACE:-"odm"}
+NAMESPACE=${NAMESPACE:-"cp4ba"}
 
 ( echo "cat <<EOF" ; cat cloud-pak-dashboard.yaml_template ;) | \
 INGRESS_DOMAIN=${INGRESS_DOMAIN} \
